c95032730acedc5d6006d3cf1f5d0e530b0bb953
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2023-01-18 16:38:26 +0900

    23/01/18

--- ServerCore/R_Program5.cs
+++ ServerCore/R_Program5.cs
@@ -6,24 +6,91 @@ namespace ServerCore
 {
     class Porgram
     {
+        class SessionManager //데드락 테스트
+        {
+            static object _lock = new object();
+
+            public static void TestSession()
+            {
+                lock( _lock )
+                {
+
+                }
+            }
+            public static void  Test()
+            {
+                lock (_lock)
+                {
+                    UserManager.Test();
+                }
+            }
+
+        }
+        class UserManager//데드락 테스트
+        {
+            static object _lock = new object();
+            public static void Test()
+            {
+
+                lock(_lock)
+                {
+                    SessionManager.TestSession();
+                }
+            }
+
+            public static void TestUser()
+            {
+                lock (_lock)
+                {
+                    
+                }
+            }
+        }
+
         static int num = 0;
+        static object obj = new object();  
 
         static void Thread1()
         {
             //int prev=num;
-            for (int i = 0; i < 1000000; i++) 
-                Interlocked.Increment(ref num);//원자적(쪼개질 수 없는 상태)으로 만들어줌-하지만 성능에서 손해를 많이봄.
-            //int afterValue = Interlocked.Increment(ref num);//다음 값 추출방법
-            //    num++;
+            for (int i = 0; i < 1000000; i++)
+            {
+                //상호 배제 Mutual Exclusive
+                //Monitor.Enter(obj);
 
+                //lock(obj) //상호배제  ,try{} finaliy
+                //{
+                //
+                //    num++;
+                //}
+
+                //    Interlocked.Increment(ref num);//원자적(쪼개질 수 없는 상태)으로 만들어줌-하지만 성능에서 손해를 많이봄.
+                //int afterValue = Interlocked.Increment(ref num);//다음 값 추출방법
+
+                //num++;
+                //Monitor.Exit(obj);
+
+                SessionManager.Test();
+            }
             //int next = num; //주소값으로 접근하기에 다음 값을 알 수 없다.
         }
 
         static void Thread2()
         {
             for (int i = 0; i < 1000000; i++)
-               // num--;
-            Interlocked.Decrement(ref num);
+            {
+                //Monitor.Enter(obj);
+                //num--;
+                //Interlocked.Decrement(ref num);
+                //Monitor.Exit(obj);
+
+                //lock(obj)
+                //{
+                //    num--;
+                //}
+
+                UserManager.Test();
+            }
         }
 
         static void Main(string[] args)
