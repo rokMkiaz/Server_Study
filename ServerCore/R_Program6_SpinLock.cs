cab765e7dd5463ff8ed1ad13216bb36b2055ea6c
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2023-01-19 14:55:55 +0900

    23/01/19

---
+++ ServerCore/R_Program6_SpinLock.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class SpinLock
+    {
+        volatile int _locked = 0;
+
+        public void Acquire()
+        {
+            while (true)
+            {
+
+                //int original = Interlocked.Exchange(ref _locked, 1);
+                //if (original == 0)break; //0이면 없는 상태, 1이면 있음
+                //잠김이 풀리기를 기다린다
+
+                //CAS Compare-And-Swap
+                int expected = 0;//C++에서는 이렇게 표시되어있음.
+                int desired = 1;
+                int original=Interlocked.CompareExchange(ref _locked, desired, expected);//좀 더 일반적인 상황
+                if (original == 0)
+                    break; 
+            }
+            //Thread.Sleep(1);//무조건 휴식 => 무조건 1ms정도 쉬고 시작
+            //Thread.Sleep(0); //조건부 양보=> 자신보다 우선순위가 낮은 곳에는 양보 불가=>우선순위가 같거나 높은 쓰래드 가없으면 자신에게 돌아옴
+            Thread.Yield(); // 관대한 양보=> 지금 실행 가능한 쓰레드가 있으면 실행=>실행 가능한게 없다면 실행            
+        }
+
+        public void Release()
+        {
+            _locked= 0; 
+        }
+    }
+    class Porgram
+    {
+        static int _num = 0;
+
+        static SpinLock _lock = new SpinLock(); 
+
+        static void Thread_1()
+        {
+            for(int i=0; i<1000000; i++)
+            {
+                _lock.Acquire();
+                _num++;
+                _lock.Release();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                _lock.Acquire();
+                _num--;
+                _lock.Release();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+            
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num) ;
+        }
+
+
+
+    }
+
+}
---
+++ ServerCore/R_Program7_SpinLock.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Lock
+    {
+
+        ManualResetEvent _available = new ManualResetEvent(true);//아무나 들어오는상태:true, 누구도 못들어오는 :false
+
+        public void Acquire()
+        {
+            _available.WaitOne();//입장시도
+        }
+
+        public void Release()
+        {
+            _available.Set();//이벤트 상태를  true로 바꿔준다
+        }//커널자체가 너무 오래걸려서 시간이 오래걸리는 것이다.
+    }
+    class Porgram
+    {
+        static int _num = 0;
+
+        static Lock _lock = new Lock();
+        static Mutex __lock = new Mutex();
+
+        static void Thread_1()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                //_lock.Acquire();
+                __lock.WaitOne();
+                
+                _num++;
+                //_lock.Release();
+                __lock.ReleaseMutex();
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                //_lock.Acquire();
+                __lock.WaitOne();
+                _num--;
+                //_lock.Release();
+                __lock.ReleaseMutex();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
+            t1.Start();
+
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(_num);
+        }
+
+
+
+    }
+
+}
