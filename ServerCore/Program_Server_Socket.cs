801ad29d978e5456ab28713c263b9dbf32b57331
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2023-01-21 17:07:44 +0900

    23/01/21

---
+++ ServerCore/Program_Server_Socket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static Listener _listener= new Listener();
+
+        static void OnAcceptHandler(Socket clientSocket)
+        {
+            try//성공여부 판단
+            {
+                Session session = new Session();
+                session.Init(clientSocket);
+ 
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+
+                session.Send(sendBuff);
+
+                Thread.Sleep(1000);
+
+                session.Disconnect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+        static void Main(string[] args)
+        {
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry iphost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = iphost.AddressList[0]; //하나 혹은 여러개의 주소를 받을 수 있음 DNS서버가 해주는것
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); //포트 번호
+
+
+
+            _listener.init(endPoint, OnAcceptHandler);
+            Console.WriteLine("Listening...");
+
+            //서버 오픈 대기
+            while (true)
+            {
+
+
+            }
+        
+
+           
+            
+          
+        }
+    }
+}
---
+++ ServerCore/Session.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+
+namespace ServerCore
+{
+    internal class Session
+    {
+        Socket _socket;
+        int _disconnected = 0;
+
+        public void Init(Socket socket)
+        {
+            _socket = socket;
+            SocketAsyncEventArgs recevArgs= new SocketAsyncEventArgs();
+            recevArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+            //recevArgs.UserToken 추가적으로 연결하고싶은게 있을 경우
+            recevArgs.SetBuffer(new byte[1024],0,1024);
+
+            RegisterRecv(recevArgs);
+        }
+
+        public void Send(byte[] sendBuff)
+        {
+            _socket.Send(sendBuff);
+        }
+
+        void RegisterRecv(SocketAsyncEventArgs args)
+        {
+            bool pending=_socket.ReceiveAsync(args);
+
+            if(pending == false)
+            {
+                OnRecvCompleted(null, args);
+            }
+        }
+        public void Disconnect()
+        {
+            if(Interlocked.Exchange(ref _disconnected, 1)==1);
+            {
+                return;
+            }
+            _socket.Shutdown(SocketShutdown.Both);
+            _socket.Close();    
+        }
+        void OnRecvCompleted(object sender,SocketAsyncEventArgs args)
+        {
+        
+            if(args.BytesTransferred>0 && args.SocketError==SocketError.Success) 
+            {
+                try
+                {
+                    string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
+                    Console.WriteLine($"[From Client]{recvData}");
+                    RegisterRecv(args);
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"OnRecvCompleted Failed {e}");
+                }
+            }
+            else
+            {
+                //TODO :: Disconnnect
+            }
+        }
+    }
+}
