1b9751b5e5aa0719f42c546bea16023b91640359
남일우 <93506849+rokMkiaz@users.noreply.github.com>
2023-01-22 14:34:09 +0900

    23/01/22

--- ServerCore/Session.cs
+++ ServerCore/Session.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Net.Sockets;
 using System.Text;
 
@@ -11,39 +12,99 @@ namespace ServerCore
         Socket _socket;
         int _disconnected = 0;
 
+        object _lock = new object();
+        Queue<byte[]> _sendQueue = new Queue<byte[]>();
+
+        List<ArraySegment<byte>> _pendingList = new List<ArraySegment<byte>>();
+        SocketAsyncEventArgs _sendArgs = new SocketAsyncEventArgs();
+        SocketAsyncEventArgs _recevArgs = new SocketAsyncEventArgs();
+
         public void Init(Socket socket)
         {
             _socket = socket;
-            SocketAsyncEventArgs recevArgs= new SocketAsyncEventArgs();
-            recevArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
+
+            _recevArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnRecvCompleted);
             //recevArgs.UserToken 추가적으로 연결하고싶은게 있을 경우
-            recevArgs.SetBuffer(new byte[1024],0,1024);
+            _recevArgs.SetBuffer(new byte[1024],0,1024);
 
-            RegisterRecv(recevArgs);
+            _sendArgs.Completed += new EventHandler<SocketAsyncEventArgs>(OnSendCompleted);
+
+            RegisterRecv();
         }
 
-        public void Send(byte[] sendBuff)
+        public void Send(byte[] sendBuff) //다음에는 패킷
         {
-            _socket.Send(sendBuff);
-        }
-
-        void RegisterRecv(SocketAsyncEventArgs args)
-        {
-            bool pending=_socket.ReceiveAsync(args);
-
-            if(pending == false)
+            lock (_lock)
             {
-                OnRecvCompleted(null, args);
+                _sendQueue.Enqueue(sendBuff);
+                if (_pendingList.Count == 0)
+                    RegisterSend();
             }
         }
         public void Disconnect()
         {
-            if(Interlocked.Exchange(ref _disconnected, 1)==1);
+            if(Interlocked.Exchange(ref _disconnected, 1)==1)
             {
                 return;
             }
             _socket.Shutdown(SocketShutdown.Both);
             _socket.Close();    
+        }
+
+        void RegisterSend( ) //Send를 할때는 다른 방식 필요
+        {
+            while (_sendQueue.Count>0) //상대방이 비정상적인 정보를 많이 보낼때 제한을 해야함.
+            {
+                byte[] buff = _sendQueue.Dequeue();
+                _pendingList.Add(new ArraySegment<byte>(buff,0,buff.Length));//struct는 스택영역
+           
+            }
+
+            _sendArgs.BufferList = _pendingList;
+
+            bool pending =  _socket.SendAsync(_sendArgs);//만들어 질때 마다 호출하는건 비효율
+            if (pending == false)
+                OnSendCompleted(null, _sendArgs);
+        }
+        void OnSendCompleted(object sender, SocketAsyncEventArgs args)
+        {
+            lock (_lock)
+            {
+
+                if (args.BytesTransferred > 0 && args.SocketError == SocketError.Success)
+                {
+                    try
+                    {
+                        _sendArgs.BufferList = null;
+                        _pendingList.Clear();//
+
+                        Console.WriteLine($"Transferred bytes:{_sendArgs.BytesTransferred}");
+
+                        if (_sendQueue.Count > 0)
+                        {
+                            RegisterSend();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine($"OnSendCompleted Failed {e}");
+                    }
+                }
+                else
+                {
+                    Disconnect();
+                }
+            }
+        }
+
+        void RegisterRecv()
+        {
+            bool pending=_socket.ReceiveAsync(_recevArgs);
+
+            if(pending == false)
+            {
+                OnRecvCompleted(null, _recevArgs);
+            }
         }
         void OnRecvCompleted(object sender,SocketAsyncEventArgs args)
         {
@@ -54,7 +115,7 @@ namespace ServerCore
                 {
                     string recvData = Encoding.UTF8.GetString(args.Buffer, args.Offset, args.BytesTransferred);
                     Console.WriteLine($"[From Client]{recvData}");
-                    RegisterRecv(args);
+                    RegisterRecv();
 
                 }
                 catch (Exception e)
@@ -64,7 +125,7 @@ namespace ServerCore
             }
             else
             {
-                //TODO :: Disconnnect
+                Disconnect();
             }
         }
     }
